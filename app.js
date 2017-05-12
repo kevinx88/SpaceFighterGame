bd3563adf444925a2dc3e22859fce5920329fc69
Kevin <kevinx88@gmail.com>
2017-05-11 21:55:12 -0400

    added timed interval

--- app.js
+++ app.js
@@ -1,32 +1,32 @@
 $(document).ready(function() {
 
+
   var ship = document.querySelector('#ship');
   var laser = document.querySelector('#laser');
   var shipCol = document.querySelectorAll('.ship-col');
   var spaceCol = document.querySelectorAll('.space-col');
   var rock = document.querySelector('#rock');
 
-  /* End of aliases */
-
-
   laser.setAttribute('data-column', '1');
   ship.setAttribute('data-column', '1');
 
-  // setInterval(function() {
-  //   var randomNo = Math.floor(Math.random() * 5);
-  //   spaceCol[randomNo].appendChild(rock);
-  // }, 1000);
+  var turnNo = 0;
 
+  var interval = setInterval(function() {
+    turnNo += 1;
+
+    if (turnNo === 5) {          //the code style here learned
+      clearInterval(interval);   //from stackoverflow
+    }                            //
+
+    var randomNo = Math.floor(Math.random() * 5);
+    spaceCol[randomNo].appendChild(rock);
+  }, 500);
 
   document.addEventListener('keydown', function(event) {
     var leftKey = event.which === 37;
     var rightKey = event.which === 39;
-
     var laserDataCol = laser.getAttribute('data-column');
-    // var laserDataCol2 = laser.getAttribute('data-column') === 2;
-    // var laserDataCol3 = laser.getAttribute('data-column') === 3;
-    // var laserDataCol4 = laser.getAttribute('data-column') === 4;
-    // var laserDataCol5 = laser.getAttribute('data-column') === 5;
 
     if (laserDataCol === '1' && rightKey) {
       return;
@@ -42,7 +42,6 @@ $(document).ready(function() {
       ship.setAttribute('data-column', col);
     }
   });
-
 
 
 });
