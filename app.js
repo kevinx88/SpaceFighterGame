331ee8493ddf210fcafd7305be48422c924cf168
Kevin <kevinx88@gmail.com>
2017-05-11 20:12:53 -0400

    random positioning

--- app.js
+++ app.js
@@ -4,9 +4,20 @@ $(document).ready(function() {
   var laser = document.querySelector('#laser');
   var shipCol = document.querySelectorAll('.ship-col');
   var spaceCol = document.querySelectorAll('.space-col');
-
+  var rock = document.querySelector('#rock');
 
   /* End of aliases */
+
+
+  laser.setAttribute('data-column', '1');
+  ship.setAttribute('data-column', '1');
+
+  // setInterval(function() {
+  //   var randomNo = Math.floor(Math.random() * 5);
+  //   spaceCol[randomNo].appendChild(rock);
+  // }, 1000);
+
+
   document.addEventListener('keydown', function(event) {
     var leftKey = event.which === 37;
     var rightKey = event.which === 39;
@@ -17,21 +28,6 @@ $(document).ready(function() {
     // var laserDataCol4 = laser.getAttribute('data-column') === 4;
     // var laserDataCol5 = laser.getAttribute('data-column') === 5;
 
-    // if (laserDataCol === '1' && leftKey) {
-    //   laser.setAttribute('data-column', '2');
-    //   ship.setAttribute('data-column', '2');
-    // } else if (laserDataCol === '5' && rightKey) {
-    //   laser.setAttribute('data-column', '4');
-    //   ship.setAttribute('data-column', '4');
-    // } else if (leftKey) {
-    //   var col = parseInt(laserDataCol) + 1 + '';
-    //   laser.setAttribute('data-column', col);
-    //   ship.setAttribute('data-column', col);
-    // } else if (rightKey) {
-    //   var col = parseInt(laserDataCol) - 1 + '';
-    //   laser.setAttribute('data-column', col);
-    //   ship.setAttribute('data-column', col);
-    // }
     if (laserDataCol === '1' && rightKey) {
       return;
     } else if (laserDataCol === '5' && leftKey) {
@@ -46,6 +42,7 @@ $(document).ready(function() {
       ship.setAttribute('data-column', col);
     }
   });
+
 
 
 });
